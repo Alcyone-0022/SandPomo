3ad9733624f9c1310f228cefbe90091f1b43d112
Alcyone <hskhsk22000@gmail.com>
2025-05-12 22:59:11 +0900

    remove unnecessary files

--- hw/build/libraries/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Seunggab\ Ha\Desktop\SandPomo\hw\build\libraries\Adafruit_NeoPixel\esp8266.c.o: \
- C:\Users\Seunggab\ Ha\Documents\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
--- hw/build/libraries/Adafruit_NeoPixel/kendyte_k210.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Seunggab\ Ha\Desktop\SandPomo\hw\build\libraries\Adafruit_NeoPixel\kendyte_k210.c.o: \
- C:\Users\Seunggab\ Ha\Documents\Arduino\libraries\Adafruit_NeoPixel\kendyte_k210.c
